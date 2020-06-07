4ff1d6ca0561b0da5499cebd35f05d065afedc9d
Khushil Khatri <khushil@quarkstudios.com>
2020-06-07 21:11:01 +0530

    Updates

---
+++ precache-manifest.f6d4c98e80d40b7b9302d42a594ffb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbe5a9e9d77a909d03c3cfd5991c785",
+    "url": "/workflow-automate-io/index.html"
+  },
+  {
+    "revision": "f47bda2ed70aa0e6c378",
+    "url": "/workflow-automate-io/static/css/main.76e6947d.chunk.css"
+  },
+  {
+    "revision": "5b0b9404ebdf81a8575e",
+    "url": "/workflow-automate-io/static/js/2.09931ec2.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/workflow-automate-io/static/js/2.09931ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47bda2ed70aa0e6c378",
+    "url": "/workflow-automate-io/static/js/main.1d21c44e.chunk.js"
+  },
+  {
+    "revision": "83e0df95fcd7139b0fdc",
+    "url": "/workflow-automate-io/static/js/runtime-main.ace8d6d8.js"
+  }
+]);
